96f727c16f0966096fbc917617a270f263eb7ffb
Shih-Chieh Chen <chen.j.shihchieh@gmail.com>
2020-11-25 21:17:50 -0800

    Able to read in email and extract sender, receiver and body. input and output on local drive

--- sparkETL.py
+++ sparkETL.py
@@ -1,18 +1,24 @@
 import sys
 from pyspark.sql import SparkSession, functions as f, types
 
+sender_regex = r'From:[^<]+<([^\n]+)>'
+receiver_regex = r'To: ([^\n]+)'
+body_regex = r'\n\n([\H\n\s]*)'
 
 
 
-
-def main():
-	email_schema = types.StructType([
-		types.StructField('wholetext', StringType()),
-	])
+def main(inputs, output):
 	
-	raw_email = spark.read.text(**textsource**, wholetext = true)
-
+	raw_email = spark.read.text(intputs, wholetext = True)
+	from_to = raw_email.select(\
+		f.regexp_extract('value', sender_regex, 1).alias('sender'), \
+		f.regexp_extract('value', receiver_regex, 1).alias('receiver'), \
+		f.regexp_extract('value', body_regex, 1).alias('body'))
+	from_to.write.csv(output, mode = 'overwrite')
+	
 if __name__ == '__main__':
+	inputs = sys.argv[0]
+	output = sys.argv[1]
     spark = SparkSession.builder.appName('email ETL').getOrCreate()
     spark.sparkContext.setLogLevel('WARN')
-    main()
+    main(inputs, output)
