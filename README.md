30529845b8d053876c7cbde58df81a18ab341e35
Shih-Chieh Chen <chen.j.shihchieh@gmail.com>
2020-11-28 11:05:50 -0800

    created simple script for starting up cassandra and updated sparkETL for communicating with cassandra

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BigSpamProject
 Jack made this. Feel free to edit and make any necessary changes. Intended to use this just for myself and happy to merge it into a proper project later but I'll be commiting my things to a separate branch instead of master just in case. 
+
+run 'cqlsh -f path/to/create_database.cql' to create the keyspace 'email_database' and create the table 'email'
+
+to run the pyspark script with cassandra on local, use the below commands
+spark-submit --packages datastax:spark-cassandra-connector:2.4.0-s_2.11 sparkETL.py DontCommit/samplemail DontCommit/out
--- sparkETL.py
+++ sparkETL.py
@@ -3,21 +3,37 @@ from pyspark.sql import SparkSession, functions as f, types
 
 sender_regex = r'From:[^<]+<([^\n]+)>'
 receiver_regex = r'To: ([^\n]+)'
+subject_regex = r'Subject: ([^\n]+)'
 body_regex = r'\n\n([\H\n\s]*)'
 
 
 
 def main(inputs, output):
 	raw_email = spark.read.text(inputs, wholetext = True)
-	from_to = raw_email.select(\
+	send_to_cassandra = raw_email.select(\
 		f.regexp_extract('value', sender_regex, 1).alias('sender'), \
 		f.regexp_extract('value', receiver_regex, 1).alias('receiver'), \
-		f.regexp_extract('value', body_regex, 1).alias('body'))
-	from_to.write.csv(output, mode = 'overwrite')
+		f.regexp_extract('value', subject_regex, 1).alias('subject'), \
+		f.regexp_extract('value', body_regex, 1).alias('body'))\
+	.withColumn('id', f.monotonically_increasing_id())
+	
+	send_to_cassandra.write.csv(output, mode='overwrite')
+	
+	send_to_cassandra.write.format("org.apache.spark.sql.cassandra")\
+		.options(table = 'email', keyspace = 'email_database').save()
+	
+	
+	
+	
+	
+	
+	
+	
 	
 if __name__ == '__main__':
 	inputs = sys.argv[1]
 	output = sys.argv[2]
-	spark = SparkSession.builder.appName('email ETL').getOrCreate()
+	spark = SparkSession.builder.appName('email ETL')\
+		.config('spark.cassandra.connection.host', 'localhost').getOrCreate()
 	spark.sparkContext.setLogLevel('WARN')
 	main(inputs, output)
