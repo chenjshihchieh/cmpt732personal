f1ebd72e8bfcc5b4b831716a5902050d344e1fbe
JiuweiW <jiuweitongxue@gmail.com>
2020-12-01 17:42:31 -0500

    add process email body

--- sparkETL.py
+++ sparkETL.py
@@ -1,4 +1,6 @@
 import sys
+import re
+import nltk, nltk.stem.porter
 from simplified_scrapy.simplified_doc import SimplifiedDoc 
 from pyspark.sql import SparkSession, functions as f, types
 
@@ -14,6 +16,44 @@ def simplify_doc_udf(html_text):
 	doc = SimplifiedDoc(html_text)
 	return doc.text
 
+def process_email_body(email_body):
+    
+    email_body = email_body.split("--", 1)
+    
+    #Removing non-ASCII characters results in a string that only contains ASCII characters.
+    encoded_string = email_body[0].encode("ascii", "ignore")
+    decode_string = encoded_string.decode()
+    
+    lower_string = decode_string.lower()
+    #Looks for any expression that starts with < and ends with > and replace and does not have any < or > in the tag it with a space
+    lower_string = re.sub('<[^<>]+>', ' ', lower_string)
+
+    # Any numbers get replaced with the string 'number'
+    lower_string = re.sub('[0-9]+', 'number', lower_string)
+
+    # Anything starting with http or https:// replaced with 'httpaddr'
+    lower_string = re.sub('(http|https)://[^\s]*', 'httpaddr', lower_string)
+
+    # Strings with "@" in the middle are considered emails --> 'emailaddr'
+    lower_string = re.sub('[^\s]+@[^\s]+', 'emailaddr', lower_string)
+
+    # The '$' sign gets replaced with 'dollar'
+    lower_string = re.sub('[$]+', 'dollar', lower_string)
+    
+    # Word list of email body
+    word_list = re.split('[@$/#.-:&\*\+=\[\]?!(){},\'\'\">_<;%\s\n\r\t]+',lower_string.strip())
+    
+    stemmer = nltk.stem.porter.PorterStemmer()
+    
+    result_list = []
+    for word in word_list:
+        if len(word) >= 1:
+            word = re.sub('[^a-zA-Z0-9]', '', word)
+            word = stemmer.stem(word)
+            result_list.append(word)
+    #print(result_list)
+        
+    
 def main(inputs, output):
 	html_to_plain_udf = f.udf(simplify_doc_udf, types.StringType())
 	raw_email = spark.read.text(inputs, wholetext = True)
@@ -26,6 +66,10 @@ def main(inputs, output):
 		f.regexp_extract('value', body_regex, 1).alias('html_body'))\
 	.withColumn('id', f.monotonically_increasing_id())\
 	.withColumn('body', html_to_plain_udf('html_body'))
+    
+    
+    
+    
 	
 	send_to_cassandra.select('id', 'sender', 'receiver', 'subject', 'body').write.csv(output, mode='overwrite')
 	
