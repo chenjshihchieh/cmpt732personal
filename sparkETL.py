67f085161cc749afa54891175ecab9394df48504
Shih-Chieh Chen <chen.j.shihchieh@gmail.com>
2020-11-30 23:41:22 -0800

    Added cassandra connection. Can write dataframes to designated cassandra db

--- sparkETL.py
+++ sparkETL.py
@@ -1,26 +1,32 @@
 import sys
+from simplified_scrapy.simplified_doc import SimplifiedDoc 
 from pyspark.sql import SparkSession, functions as f, types
 
 sender_regex = r'From:[^<]+<([^\n]+)>'
 receiver_regex = r'To: ([^\n]+)'
 subject_regex = r'Subject: ([^\n]+)'
 body_regex = r'\n\n([\H\n\s]*)'
+cassandra_host = 'localhost'
 
-
+def simplify_doc_udf(html_text):
+	doc = SimplifiedDoc(html_text)
+	return doc.text
 
 def main(inputs, output):
+	html_to_plain_udf = f.udf(simplify_doc_udf, types.StringType())
 	raw_email = spark.read.text(inputs, wholetext = True)
 	send_to_cassandra = raw_email.select(\
 		f.regexp_extract('value', sender_regex, 1).alias('sender'), \
 		f.regexp_extract('value', receiver_regex, 1).alias('receiver'), \
 		f.regexp_extract('value', subject_regex, 1).alias('subject'), \
-		f.regexp_extract('value', body_regex, 1).alias('body'))\
-	.withColumn('id', f.monotonically_increasing_id())
+		f.regexp_extract('value', body_regex, 1).alias('html_body'))\
+	.withColumn('id', f.monotonically_increasing_id())\
+	.withColumn('body', html_to_plain_udf('html_body'))
 	
-	send_to_cassandra.write.csv(output, mode='overwrite')
+	send_to_cassandra.select('id', 'sender', 'receiver', 'subject', 'body').write.csv(output, mode='overwrite')
 	
-	send_to_cassandra.write.format("org.apache.spark.sql.cassandra")\
-		.options(table = 'email', keyspace = 'email_database').save()
+	#send_to_cassandra.write.format("org.apache.spark.sql.cassandra")\
+	#	.options(table = 'email', keyspace = 'email_database').save()
 	
 	
 	
@@ -34,6 +40,6 @@ if __name__ == '__main__':
 	inputs = sys.argv[1]
 	output = sys.argv[2]
 	spark = SparkSession.builder.appName('email ETL')\
-		.config('spark.cassandra.connection.host', 'localhost').getOrCreate()
+		.config('spark.cassandra.connection.host', cassandra_host).getOrCreate()
 	spark.sparkContext.setLogLevel('WARN')
 	main(inputs, output)
