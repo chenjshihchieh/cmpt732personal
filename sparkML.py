ce8caf32efd8339f284042573d45666d6249b740
Jack Chen <chen.j.shihchieh@gmail.com>
2020-12-08 16:38:43 -0800

    mend

--- sparkML.py
+++ sparkML.py
@@ -1,6 +1,8 @@
 import sys
 from pyspark.sql import SparkSession, functions as f
 from pyspark.ml.feature import HashingTF
+from pyspark.ml.classification import LinearSVC
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
 cassandra_host = 'localhost'
 
@@ -9,28 +11,20 @@ def main(mode):
     raw = spark.read.format("org.apache.spark.sql.cassandra") \
 		.options(table="emails", keyspace"email_database").load()
     
-    hashingWords = HashingTF(inputCol='word_tokens', outputCol='features')
-    hashingWords.setNumFeatures(1000)
-    
-##Create a list of all the words from all the email
-    distinct_words = raw.withColumns('distinct', f.array_distinct(raw['word_tokens']))
-    all_words_with_repeat = distinct_words.select(f.collect_set('distinct')).collect()
-    all_words = []
-    for sublist in all_words_with_repeat[0][0]:
-        for item in sublist:
-            all_words.append(item)
-    words_set = set(all_words)
-
+    hashingTransformer = HashingTF(inputCol='word_tokens', outputCol='features')
+    hashingTransformer.setNumFeatures(1000)
+    SVC = LinearSVC(featuresCol='features', labelCol='labels', predictionCol='prediction')
+    SVC_pipeline = Pipeline(states=[hashingTransformer, LinearSVC])
 
     train, validation = raw.randomSplit([0.75, 0.25])
     train = train.cache()
-	validation = validation.cache()`
-    n = 
-    
+	validation = validation.cache()
+    SVC_model = SVC_pipeline.fit(train)
+    prediction = SVC_model.transform(balidation)
 
 
 if__name__ == 'main':
-    spark = SparkSession.builder.appName('email ETL')\
+    spark = SparkSession.builder.appName('emailML')\
 		.config('spark.cassandra.connection.host', cassandra_host).getOrCreate()
     spark.sparkContext.setLogLevel('WARN')
 	main()
